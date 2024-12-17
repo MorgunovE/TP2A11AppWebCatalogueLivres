ce1ac402a46cc701fc04cde33b38ce9e3454dde9
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-12-17 12:58:17 -0500

    add netBeans files

---
+++ src/main/java/edu/a11/appwebcataloguelivres/tp2a11appwebcataloguelivres/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package edu.a11.appwebcataloguelivres.tp2a11appwebcataloguelivres.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
