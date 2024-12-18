a44003f3479f51e0aa200a9e6c811ffa8455d006
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-12-17 19:25:58 -0500

    add BasketService

---
+++ src/main/java/service/BasketService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author 1
+ */
+
+mport DAL.BasketDAO_JPA;
+import DAL.IDAO;
+import model.Basket;
+
+import java.util.List;
+
+public class BasketService {
+    private IDAO<Basket> basketDAO = new BasketDAO_JPA();
+
+    public void createBasket(Basket basket) {
+        basketDAO.create(basket);
+    }
+
+    public Basket findBasketById(Long id) {
+        return basketDAO.findById(id);
+    }
+
+    public List<Basket> findAllBaskets() {
+        return basketDAO.findAll();
+    }
+
+    public void updateBasket(Basket basket) {
+        basketDAO.update(basket);
+    }
+
+    public void deleteBasket(Basket basket) {
+        basketDAO.delete(basket);
+    }
+}
