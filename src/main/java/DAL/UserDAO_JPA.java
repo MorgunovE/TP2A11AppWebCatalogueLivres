06d5bfb354dec97a5ebd3e488b99002b219587b2
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-12-18 09:25:05 -0500

    change UserDAO_JPA

--- src/main/java/DAL/UserDAO_JPA.java
+++ src/main/java/DAL/UserDAO_JPA.java
@@ -11,6 +11,7 @@ package DAL;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import jakarta.persistence.spi.PersistenceUnitInfo;
 import model.User;
 import org.hibernate.jpa.HibernatePersistenceProvider;
@@ -33,9 +34,14 @@ public class UserDAO_JPA implements IDAO<User> {
 
     @Override
     public void create(User user) {
-        em.getTransaction().begin();
-        em.persist(user);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -52,15 +58,71 @@ public class UserDAO_JPA implements IDAO<User> {
 
     @Override
     public void update(User user) {
-        em.getTransaction().begin();
-        em.merge(user);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.merge(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(User user) {
-        em.getTransaction().begin();
-        em.remove(em.contains(user) ? user : em.merge(user));
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.remove(em.contains(user) ? user : em.merge(user));
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Trouve un utilisateur par son nom et prénom.
+     *
+     * @param name      le nom de l'utilisateur
+     * @param familyName le prénom de l'utilisateur
+     * @return l'utilisateur trouvé, ou null en cas d'erreur
+     */
+    public List<User> findByNameAndFamilyName(String name,
+                                              String familyName) {
+        TypedQuery<User> query = em
+                .createQuery("SELECT u FROM User u " +
+                        "WHERE u.name = :name AND " +
+                        "u.familyName = :familyName", User.class);
+        query.setParameter("name", name);
+        query.setParameter("familyName", familyName);
+        return query.getResultList();
+    }
+
+    /**
+     * Trouve un utilisateur par son nom.
+     *
+     * @param tel le tel de l'utilisateur
+     * @return l'utilisateur trouvé, ou null en cas d'erreur
+     */
+    public List<User> findByTel(String tel) {
+        TypedQuery<User> query = em
+                .createQuery("SELECT u FROM User u " +
+                        "WHERE u.tel = :tel", User.class);
+        query.setParameter("tel", tel);
+        return query.getResultList();
+    }
+
+    /**
+     * Trouve un utilisateur par son email.
+     *
+     * @param email l'email de l'utilisateur
+     * @return l'utilisateur trouvé, ou null en cas d'erreur
+     */
+    public List<User> findByEmail(String email) {
+        TypedQuery<User> query = em
+                .createQuery("SELECT u FROM User u " +
+                        "WHERE u.email = :email", User.class);
+        query.setParameter("email", email);
+        return query.getResultList();
     }
 }
