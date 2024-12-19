d2d428acb6f48c2ee788599d9b2add8f7b594df2
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-12-18 19:51:36 -0500

    change LivreDAO_JPA

--- src/main/java/DAL/LivreDAO_JPA.java
+++ src/main/java/DAL/LivreDAO_JPA.java
@@ -10,28 +10,32 @@ package DAL;
  */
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.TypedQuery;
-import jakarta.persistence.spi.PersistenceUnitInfo;
 import model.Livre;
-import org.hibernate.jpa.HibernatePersistenceProvider;
-
-import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Classe LivreDAO_JPA.
+ */
 public class LivreDAO_JPA implements IDAO<Livre> {
+
+    /**
+     * L'entity manager.
+     */
     private EntityManager em;
 
+    /**
+     * Constructeur de la classe LivreDAO_JPA.
+     */
     public LivreDAO_JPA() {
-        PersistenceUnitInfo catalogPUInfo =
-                new CatalogPersistenceUnit();
-        EntityManagerFactory factory =
-                new HibernatePersistenceProvider()
-                .createContainerEntityManagerFactory(catalogPUInfo,
-                        new HashMap<>());
-        this.em = factory.createEntityManager();
+        this.em = EM_Provider.getInstance().getEntityManager();
     }
 
+    /**
+     * Crée un livre.
+     *
+     * @param livre le livre à créer
+     */
     @Override
     public void create(Livre livre) {
         try {
@@ -44,18 +48,34 @@ public class LivreDAO_JPA implements IDAO<Livre> {
         }
     }
 
+    /**
+     * Trouve un livre par son identifiant.
+     *
+     * @param id l'identifiant du livre
+     * @return le livre trouvé, ou null en cas d'erreur
+     */
     @Override
     public Livre findById(Long id) {
         return em.find(Livre.class, id);
     }
 
+    /**
+     * Trouve tous les livres.
+     *
+     * @return la liste de tous les livres, ou null en cas d'erreur
+     */
     @Override
     public List<Livre> findAll() {
-        return em
-                .createQuery("SELECT l FROM Livre l",
-                        Livre.class).getResultList();
+        TypedQuery<Livre> query = em
+                .createQuery(SQL_BOX.FIND_ALL_LIVRES_JPQL, Livre.class);
+        return query.getResultList();
     }
 
+    /**
+     * Met à jour un livre.
+     *
+     * @param livre le livre à mettre à jour
+     */
     @Override
     public void update(Livre livre) {
         try {
@@ -68,6 +88,11 @@ public class LivreDAO_JPA implements IDAO<Livre> {
         }
     }
 
+    /**
+     * Supprime un livre.
+     *
+     * @param livre le livre à supprimer
+     */
     @Override
     public void delete(Livre livre) {
         try {
@@ -80,34 +105,59 @@ public class LivreDAO_JPA implements IDAO<Livre> {
         }
     }
 
+    /**
+     * Trouve un livre par son titre.
+     *
+     * @param title le titre du livre
+     * @return le livre trouvé, ou null en cas d'erreur
+     */
     public List<Livre> findByTitle(String title) {
         TypedQuery<Livre> query = em
-                .createQuery("SELECT l FROM Livre l " +
-                        "WHERE l.title = :title", Livre.class);
+                .createQuery(SQL_BOX.FIND_LIVRE_BY_TITLE_JPQL,
+                        Livre.class);
         query.setParameter("title", title);
         return query.getResultList();
     }
 
+    /**
+     * Trouve un livre par son auteur.
+     *
+     * @param author l'auteur du livre
+     * @return le livre trouvé, ou null en cas d'erreur
+     */
     public List<Livre> findByAuthor(String author) {
         TypedQuery<Livre> query = em
-                .createQuery("SELECT l FROM Livre l " +
-                        "WHERE l.author = :author", Livre.class);
+                .createQuery(SQL_BOX.FIND_LIVRE_BY_AUTHOR_JPQL,
+                        Livre.class);
         query.setParameter("author", author);
         return query.getResultList();
     }
 
+    /**
+     * Trouve un livre par son genre.
+     *
+     * @param genre le genre du livre
+     * @return le livre trouvé, ou null en cas d'erreur
+     */
     public List<Livre> findByGenre(String genre) {
         TypedQuery<Livre> query = em
-                .createQuery("SELECT l FROM Livre l " +
-                        "WHERE l.genre = :genre", Livre.class);
+                .createQuery(SQL_BOX.FIND_LIVRE_BY_GENRE_JPQL,
+                        Livre.class);
         query.setParameter("genre", genre);
         return query.getResultList();
     }
 
+    /**
+     * Trouve les livres par prix.
+     *
+     * @param price le prix du livre
+     * @return la liste des livres trouvés, ou null en cas d'erreur
+     */
     public List<Livre> findByPriceLessThanOrEqual(double price) {
         TypedQuery<Livre> query = em
-                .createQuery("SELECT l FROM Livre l " +
-                        "WHERE l.price <= :price", Livre.class);
+                .createQuery(SQL_BOX.
+                        FIND_LIVRE_BY_PRICE_LESS_THAN_OR_EQUAL_JPQL,
+                        Livre.class);
         query.setParameter("price", price);
         return query.getResultList();
     }
