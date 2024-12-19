319051c8b2cda49c6dd45a8aecda35aa594d046c
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-12-18 19:51:23 -0500

    change IDAO

--- src/main/java/DAL/IDAO.java
+++ src/main/java/DAL/IDAO.java
@@ -6,15 +6,41 @@ package DAL;
  */
 
 import java.util.List;
+
 /**
  * Interface for Data Access Object
  * @param <T> entity type
  */
-
 public interface IDAO<T> {
+
+    /**
+     * Create an entity
+     * @param entity the entity to create
+     */
     void create(T entity);
+
+    /**
+     * Find an entity by its id
+     * @param id the id of the entity
+     * @return the entity found, or null in case of error
+     */
     T findById(Long id);
+
+    /**
+     * Find all entities
+     * @return the list of all entities, or null in case of error
+     */
     List<T> findAll();
+
+    /**
+     * Update an entity
+     * @param entity the entity to update
+     */
     void update(T entity);
+
+    /**
+     * Delete an entity
+     * @param entity the entity to delete
+     */
     void delete(T entity);
 }
